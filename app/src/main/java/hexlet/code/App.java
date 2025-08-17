91a320db6d9e0b2d3c1386f18e0e810d3c0ca7d8
nikita.lozhka <nikita.lozhka@glowbyteconsulting.com>
2025-08-18 00:09:07 +0300

    fix: codestyle

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,6 +1,11 @@
 package hexlet.code;
 
-import hexlet.code.games.*;
+import hexlet.code.games.CalcGame;
+import hexlet.code.games.GcdGame;
+import hexlet.code.games.GreatGame;
+import hexlet.code.games.EvenGame;
+import hexlet.code.games.ProgressionGame;
+import hexlet.code.games.PrimeGame;
 
 import java.io.PrintStream;
 import java.util.function.Supplier;
